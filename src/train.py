13f09052061d63718e2e848e392c1dab60c2cae5
Samecho <zhayutong@gmail.com>
2025-10-17 19:43:27 -0700

    feat: add MLflow to train.py

--- src/train.py
+++ src/train.py
@@ -4,10 +4,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, recall_score, precision_score, f1_score, roc_auc_score
 from imblearn.over_sampling import SMOTE
 
-import joblib
+import mlflow
+import mlflow.sklearn
 
 print("Loading dataset...")
 creditcard = pd.read_csv('data/raw/creditcard.csv')
@@ -29,16 +30,37 @@ print("Oversampling dataset...")
 smote = SMOTE(random_state=42)
 X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
 
-print("Training model...")
-final_model = RandomForestClassifier(n_estimators=100, random_state=0, n_jobs=-1)
-final_model.fit(X_train_smote, y_train_smote)
+with mlflow.start_run():
+    n_estimators = 100
+    random_state = 0
+    
+    print("Saving parameters to MLflow...")
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("random_state", random_state)
+    mlflow.log_param("oversampling_method", "SMOTE")
 
-print("Evaluating model...")
-y_pred = final_model.predict(X_test)
-print("Model report:")
-print(classification_report(y_test, y_pred))
+    print("Training model...")
+    final_model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state, n_jobs=-1)
+    final_model.fit(X_train_smote, y_train_smote)
 
-print("Saving model to models/best_model.pkl file...")
-joblib.dump(final_model, 'models/best_model.pkl')
+    print("Evaluating model...")
+    y_pred = final_model.predict(X_test)
+    y_pred_proba = final_model.predict_proba(X_test)[:, 1]
+    recall = recall_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    auc = roc_auc_score(y_test, y_pred_proba)
 
+    print("Saving metrics to MLflow...")
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("f1_score", f1) 
+    mlflow.log_metric("auc", auc)
+
+    print("Saving model to MLflow...")
+    mlflow.sklearn.log_model(final_model, name="random_forest_model")
+
+    print("Model report:")
+    print(classification_report(y_test, y_pred))
+ 
 print("Execution complete!")
